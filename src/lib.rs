ac798bbb4a676a1b590795a0c05167a6703805f0
Jacob Kiesel <kieseljake@gmail.com>
2018-04-29 10:23:46 -0600

    Format and bump version

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // 	http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,14 +16,14 @@
 //! based on the platform being targeted.  Can be used on any `Sized` type.
 //!
 //! This is guaranteed to be a zero cost abstraction, as all calls are inlined.
-//! 
+//!
 //! ```
 //! extern crate platform;
 //!
 //! use platform::Platform;
 //!
 //! fn main() {
-//!     println!("Hello from {}!", 
+//!     println!("Hello from {}!",
 //!         "unknown"
 //!         .ios("ios")
 //!         .android("android")
@@ -39,132 +39,132 @@
 pub trait Platform: Sized {
     #[inline(always)]
     fn ios(self, _input: Self) -> Self {
-        #[cfg(target_os="ios")]
+        #[cfg(target_os = "ios")]
         {
             _input
         }
-        
-        #[cfg(not(target_os="ios"))]
+
+        #[cfg(not(target_os = "ios"))]
         {
             self
         }
     }
     #[inline(always)]
     fn android(self, _input: Self) -> Self {
-        #[cfg(target_os="android")]
+        #[cfg(target_os = "android")]
         {
             _input
         }
-        
-        #[cfg(not(target_os="android"))]
+
+        #[cfg(not(target_os = "android"))]
         {
             self
         }
     }
     #[inline(always)]
     fn windows(self, _input: Self) -> Self {
-        #[cfg(target_os="windows")]
+        #[cfg(target_os = "windows")]
         {
             _input
         }
-        
-        #[cfg(not(target_os="windows"))]
+
+        #[cfg(not(target_os = "windows"))]
         {
             self
         }
     }
     #[inline(always)]
     fn macos(self, _input: Self) -> Self {
-        #[cfg(target_os="macos")]
+        #[cfg(target_os = "macos")]
         {
             _input
         }
-        
-        #[cfg(not(target_os="macos"))]
+
+        #[cfg(not(target_os = "macos"))]
         {
             self
         }
     }
     #[inline(always)]
     fn linux(self, _input: Self) -> Self {
-        #[cfg(target_os="linux")]
+        #[cfg(target_os = "linux")]
         {
             _input
         }
-        
-        #[cfg(not(target_os="linux"))]
+
+        #[cfg(not(target_os = "linux"))]
         {
             self
         }
     }
     #[inline(always)]
     fn wasm32(self, _input: Self) -> Self {
-        #[cfg(target_arch="wasm32")]
-            {
-                _input
-            }
-
-        #[cfg(not(target_arch="wasm32"))]
-            {
-                self
-            }
-    }
-    #[inline(always)]
-    fn emscripten(self, _input: Self) -> Self {
-        #[cfg(target_os="emscripten")]
+        #[cfg(target_arch = "wasm32")]
         {
             _input
         }
-        
-        #[cfg(not(target_os="emscripten"))]
+
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            self
+        }
+    }
+    #[inline(always)]
+    fn emscripten(self, _input: Self) -> Self {
+        #[cfg(target_os = "emscripten")]
+        {
+            _input
+        }
+
+        #[cfg(not(target_os = "emscripten"))]
         {
             self
         }
     }
     #[inline(always)]
     fn freebsd(self, _input: Self) -> Self {
-        #[cfg(target_os="freebsd")]
+        #[cfg(target_os = "freebsd")]
         {
             _input
         }
 
-        #[cfg(not(target_os="freebsd"))]
+        #[cfg(not(target_os = "freebsd"))]
         {
             self
         }
     }
     #[inline(always)]
     fn openbsd(self, _input: Self) -> Self {
-        #[cfg(target_os="openbsd")]
+        #[cfg(target_os = "openbsd")]
         {
             _input
         }
 
-        #[cfg(not(target_os="openbsd"))]
+        #[cfg(not(target_os = "openbsd"))]
         {
             self
         }
     }
     #[inline(always)]
     fn dragonfly(self, _input: Self) -> Self {
-        #[cfg(target_os="dragonfly")]
+        #[cfg(target_os = "dragonfly")]
         {
             _input
         }
 
-        #[cfg(not(target_os="dragonfly"))]
+        #[cfg(not(target_os = "dragonfly"))]
         {
             self
         }
     }
     #[inline(always)]
     fn netbsd(self, _input: Self) -> Self {
-        #[cfg(target_os="netbsd")]
+        #[cfg(target_os = "netbsd")]
         {
             _input
         }
 
-        #[cfg(not(target_os="netbsd"))]
+        #[cfg(not(target_os = "netbsd"))]
         {
             self
         }
